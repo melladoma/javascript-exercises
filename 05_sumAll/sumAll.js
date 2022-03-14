e990d21e819f9da60391ca0e60c94a0b6cbf1141
Marine Mellado <marine.mellado@gmail.com>
2022-03-14 18:50:50 +0100

    solved exs 5 to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let result = 0;
 
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0) {
+        result = "ERROR";
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            result += i;
+        }
+    } else if (num2 < num1) {
+        for (let i = num2; i <= num1; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
-
+console.log(sumAll(-1, 4))
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 100 !== 0 && year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(leapYears(1600))
 // Do not edit below this line
 module.exports = leapYears;
