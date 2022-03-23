788687ff33540824998fc2931825a34e537f6c34
Marine Mellado <marine.mellado@gmail.com>
2022-03-23 11:42:44 +0100

    completed ex 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function ([...theArgs]) {
+  return theArgs.reduce((previous, current) => previous + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function ([...theArgs]) {
+  return theArgs.reduce((previous, current) => previous * current, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1
+  } else {
+    for (let i = a - 1; i > 0; i--) {
+      a *= i
+    }
+    return a;
+  }
 };
+
+// another implementation of Factorial that uses recursion from the solutions
+// const recursiveFactorial = function(n) {
+//   if (n === 0) {
+//     return 1;
+//   }
+//   return n * recursiveFactorial (n-1);
+// };
 
 // Do not edit below this line
 module.exports = {
