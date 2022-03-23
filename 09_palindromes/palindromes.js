40b6642092c3d33bce23b47b733351ae314f3f9c
Marine Mellado <marine.mellado@gmail.com>
2022-03-23 13:49:09 +0100

    completed ex9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuationless = str.replace(/[.,!\s]/g, "").toLowerCase();
+    let splitstr = punctuationless.split("").reverse().join("");
+    return punctuationless === splitstr
 };
 
 // Do not edit below this line
